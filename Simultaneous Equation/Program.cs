3b75c0fc583614088fd8a2c40b2182568cc0c346
TheBowale <adewaleadebowale1000@gmail.com>
2021-07-12 08:00:23 +0000

    Assignment 2

---
+++ Simultaneous Equation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Simultaneous_Equation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solving a simultaneous equation using cramers rule;\n ax + by = e \n cx + dy = f");
+            Console.WriteLine("Enter your value for a, b, e, c, d, f");
+            Console.WriteLine("a: ");
+            double a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b: ");
+            double b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("e: ");
+            double e = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("c: ");
+            double c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("d: ");
+            double d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("f: ");
+            double f = Convert.ToInt32(Console.ReadLine());
+
+            double x = (e * d - b * f) / (a * d - b * c);
+            double y = (a * f - e * c) / (a * d - b * c);
+
+            if (a * d - b * c == 0)
+            {
+                Console.WriteLine("Invalid");
+            }
+            else
+            {
+                Console.WriteLine("X is " + x);
+                Console.WriteLine("Y is " + y);
+            }
+
+
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are you a Fulltime or Part-time student? : ");
+
+            string student = Console.ReadLine();
+            student = student.ToLower();
+
+            if (student == "fulltime")
+            {
+                Console.WriteLine("What is your Course of Study? : ");
+                string course = Console.ReadLine();
+                Console.WriteLine(course + " is a good course. ");
+            }
+            else if (student == "part-time")
+            {
+                Console.WriteLine("How many credits are you taking? ; ");
+                int credits = Convert.ToInt32(Console.ReadLine());
+                if (credits > 6)
+                {
+                    Console.WriteLine("That's too much unit for a part-time student");
+                }
+                else
+                {
+                    Console.WriteLine("Proceed to Screen");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Entry");
+            }
+        }
+    }
+}
+
